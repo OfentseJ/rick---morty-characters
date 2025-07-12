ca6fb8e45fbb62035b5371ca5fb8d2f9b0c4cad7
Ofentse <ofentsemakhutja@icloud.com>
2025-07-12 12:42:12 +0200

    Add custom font and redesign character display
    Added WubbaLubbaDubDub font files and applied the custom font via styles.css. Refactored index.html and script.js to improve the Rick & Morty character display, using a table layout for details and updating button placement. Fixed stylesheet link in index.html and added new CSS for layout and styling.

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-const imageContainer = document.getElementById("image_container");
-const nameP = document.getElementById("name");
-const statusSpan = document.getElementById("status");
-const speciesSpan = document.getElementById("species");
-const typeSpan = document.getElementById("type");
-const genderSpan = document.getElementById("gender");
-const locationSpan = document.getElementById("location");
+const container = document.getElementById("container");
 
 let index = 0;
 
 const display = async (index) => {
   try {
-    const data = await fetch(
-      "https://rickandmortyapi.com/api/character/?count=40"
-    )
+    const data = await fetch("https://rickandmortyapi.com/api/character")
       .then((res) => {
         if (!res.ok) {
           throw new Error("Could not retrieve data");
@@ -20,21 +12,42 @@ const display = async (index) => {
         return res.json();
       })
       .catch((error) => console.error(error));
-    const name = data.results[index].name;
-    const status = data.results[index].status;
-    const species = data.results[index].species;
-    const type = data.results[index].type;
-    const gender = data.results[index].gender;
-    const location = data.results[index].location;
-    const image = data.results[index].image;
 
-    imageContainer.innerHTML = `<img src='${image}' alt='An image of ${name}'/>`;
-    nameP.innerText = name;
-    statusSpan.innerText = status;
-    speciesSpan.innerText = species;
-    typeSpan.innerText = type;
-    genderSpan.innerText = gender;
-    location.innerText = location;
+    const { name, status, species, gender, origin, image } =
+      data.results[index];
+
+    container.innerHTML = `
+      <div class="image-container" id="image_container">
+        <img src='${image}' alt='An image of ${name}'/>
+      </div>
+      <div class="details">
+      <table>
+        <tr>
+          <td>Name:</td>
+          <td>${name}</td>
+        </tr>
+        <tr>
+          <td>Status:</td>
+          <td>${status}</td>
+        </tr>
+        <tr>
+          <td>Species:</td>
+          <td>${species}</td>
+        </tr>
+        <tr>
+          <td>Gender:</td>
+          <td>${gender}</td>
+        </tr>
+        <tr>
+          <td>Origin:s</td>
+          <td>${origin.name}</td>
+        </tr>
+      </table>
+      </div>
+      <div class="btns">
+        <button onclick="backBtn()">back</button>
+        <button onclick="nextBtn()">next</button>
+      </div>`;
   } catch (error) {
     console.log("Error:", error);
   }
